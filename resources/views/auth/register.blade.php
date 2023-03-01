5d6dd5bbfd8582c14767096bc16a73a46ccb508f
QuantumBen <omosabenedict@gmail.com>
2023-03-01 16:15:29 +0100

    Registration layout for teacher

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,6 +39,23 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <!-- Role -->
+        <div class="mt-4">
+            <x-input-label for="role_id" :value="__('Register as')" />
+
+            <label>
+                <input type="radio" name="role_id" value="1" checked /> Student
+            </label>
+
+            <label class="ml-2">
+                <input type="radio" name="role_id" value="2" checked /> Teacher
+            </label>
+
+
+            <x-input-error :messages="$errors->get('role_id')" class="mt-2" />
+        </div>
+
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
